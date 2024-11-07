b49e4dcd963e336569dbbf9bafe7936eeffddf3b
Ulrike <ulrikebloch@icloud.com>
2024-11-07 12:28:06 +0700

    deployed on sepolia

--- components/EntryForm.js
+++ components/EntryForm.js
@@ -38,7 +38,7 @@ const EntryForm = () => {
                 const provider = new ethers.BrowserProvider(window.ethereum);
                 const signer = await provider.getSigner();
                 
-                const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+                const contractAddress = '0x02C4bCE808937Ef2Ace44F89557Bb8cD217D3473';
                 console.log('Contract address:', contractAddress);
                 
                 const contractInstance = new ethers.Contract(
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.0",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
